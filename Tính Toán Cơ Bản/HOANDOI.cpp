f3a3f62252297e4a67a40c17b039e32aca26bc07
btrunghieu <buitrunghieu11322004htt@gmail.com>
2022-12-08 15:57:27 +0700

    update HOANDOI file

---
+++ Tính Toán Cơ Bản/HOANDOI.cpp
@@ -0,0 +1,19 @@
+//buitrunghieu
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pi pair<int, int>
+#define fi first
+#define se second
+
+int main(){
+    //freopen("in.inp","r", stdin);
+    //freopen("out.out","w", stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll a, b; cin >> a >> b;
+    cout << b << " " << a;
+    return 0;
+}
+    
