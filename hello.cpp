25d6f180941cc947e89dd282a57dceda9184f010
btrunghieu <buitrunghieu11322004htt@gmail.com>
2022-12-27 14:11:29 +0700

    update test

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+//buitrunghieu
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define pi pair<int, int>
+#define fi first
+#define se second
+
+int main(){
+    //freopen("in.inp","r", stdin);
+    //freopen("out.out","w", stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
